3a0b986bfc83807d84ecc829e185cca9f7de49ea
esalagran <esalagran@gmail.com>
2019-03-14 17:52:29 +0100

    Try to set up the beginnig

---
+++ Presentation/CtrlPresentation.java
@@ -0,0 +1,15 @@
+package Presentation;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.*;
+
+public class CtrlPresentation {
+
+    private CtrlDomain CD = new CtrlDomain();
+
+    int main(){
+        
+    }
+
+}
