c487a9567e8dfc627119bfa5621add861eb46a0d
esalagran <esalagran@gmail.com>
2019-03-24 11:57:09 +0100

    Inicialització de la classe FitxaProblema
    Crec que és millor fer la comprovació de si una peça es pot moure en el problema mateix perquè és on hi ha el problema carregat

--- src/Domain/FitxaProblema.java
+++ src/Domain/FitxaProblema.java
@@ -1,16 +1,52 @@
 package Domain;
 
-public class FitxaProblema {
+public class FitxaProblema{
+
+
 
     IFitxa fitxa;
-    Problema prob;
     boolean negre;
-    ParInt[] coord;
+    ParInt coord;
+
+    public String GetNom(){
+        return fitxa.GetNom();
+    };
 
 
 
     void MourePeça(int a, boolean b, ParInt desti){
 
+    }
+
+    FitxaProblema(String nom, ParInt posIni, boolean color){
+        CreaFitxa(nom);
+        coord = posIni;
+        negre = color;
+    }
+
+    private void CreaFitxa(String nom){
+        switch (nom){
+            case "Peo":
+                fitxa = new Peo();
+                break;
+            case "Cavall":
+                fitxa = new Cavall();
+                break;
+            case "Alfil":
+                fitxa = new Alfil();
+                break;
+            case "Torre":
+                fitxa = new Torre();
+                break;
+            case "Dama":
+                fitxa =  new Dama();
+                break;
+            case "Rei":
+                fitxa = new Rei();
+                break;
+            default:
+                throw new IllegalArgumentException("El nom de la peça no és correcte");
+        }
     }
 
 }
