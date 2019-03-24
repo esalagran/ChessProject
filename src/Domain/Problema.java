a564ee048121f32f4f36e06e4821735b71debe52
oriolpascual9 <43992574+oriolpascual9@users.noreply.github.com>
2019-03-24 14:44:36 +0100

    Add files via upload

--- src/Domain/Problema.java
+++ src/Domain/Problema.java
@@ -1,15 +1,89 @@
+/**
+ * @file Problema.java
+ * @brief
+ *
+ * @autor Oriol
+ */
+
 package Domain;
 
 import java.util.HashMap;
 
+/**
+ * @class Problema
+ * \brief
+ */
+
 public class Problema{
-    int id;
-    String FEN;
-    Dificultat dif;
-    boolean guardat;
-    boolean valid;
-    Huma creador;
-    HashMap<String, Integer> ranking;
+    private int id;
+    private String FEN;
+    private Dificultat dif;
+    private boolean guardat;
+    private boolean valid;
+    private Huma creador;
+    private HashMap<String, Integer> ranking;
+    private vector FitxaProblema;
+
+    /**
+     * Constructora buida
+     */
+    public Problema() {}
+
+    /**
+     * Constructora 2
+     */
+    public Problema (int a, String b, Dificultat c, Huma d){
+        id = a;
+        FEN = b;
+        dif = c;
+        creador = d;
+    }
+
+    public void setDificultat (Dificultat d){
+        dif = d;
+    }
+
+    public void validarProblema (){
+        Maquina aux;
+        valid = aux.validarProblema(id);
+    }
+
+    public HashMap<String, Integer> getRanking() {
+        return ranking;
+    }
+
+    //RANKING FUNCTIONS
+
+    /**
+     * Si el jugador ja existeix en el ranking actualiza la seva puntacio,
+     * sino crea una nova instancia amb el nickname i la puntuacio
+     * @param nickname Nom del jugador a inscriure al ranking
+     * @param puntuacio Puntuacio del jugador en el problema
+     */
+    public void inscriureRanking(String nickname, Integer puntuacio) {
+        //Replaces the entry for the specified key only if it is currently mapped to some value, otherwise returns NULL
+        if (ranking.replace (nickname, puntuacio) == NULL) ranking.put(nickname,puntuacio);
+    }
+
+    public Integer consultarPuntuacioJugador(String nickname) {
+        return ranking.get(nickname);
+    }
+
+    //PRIVATES
+
+    /**
+     * A partir del codi FEN genera el vector de FitxaProblema
+     */
+    private void FENtoTauler(){
+
+    }
+
+    /**
+     * A partir del vector de FitxaProblema genera el codi FEN
+     */
+    private void TaulertoFEN(){
+
+    }
 }
 
 
